07de0cf8b840d48c01bae793b357529c765909cd
aline-bebe <ingabirealine40@gmail.com>
2019-10-17 14:44:33 +0200

    install decouple and adding forms

--- insta/settings.py
+++ insta/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'bootstrap3',
+    'instagram',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- instagram/views.py
+++ instagram/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
-
-from django.http  import HttpResponse
+from .forms import NewsLetterForm
+from django.http import HttpResponse, Http404,HttpResponseRedirect
 
 # Create your views here.
 def welcome(request):
     return HttpResponse('Welcome to instagram')
+
+def insta(request):
+    if request.method == 'POST':
+        form = NewsLetterForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['your_name']
+            email = form.cleaned_data['email']
+            recipient = NewsLetterRecipients(name = name,email =email)
+            recipient.save()
+            HttpResponseRedirect('insta')
+    else:
+        form = NewsLetterForm()
+    return render(request, 'all-insta/insta.html', {"letterForm":form})
